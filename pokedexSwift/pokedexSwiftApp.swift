e136057ce5addc781b4d2ab347639937d321414a
Hugo Restrepo <hrestrepo@libelulasoft.com>
2024-02-07 12:14:14 -0500

    Initial Commit

---
+++ pokedexSwift/pokedexSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexSwiftApp.swift
+//  pokedexSwift
+//
+//  Created by LibelulaSoft Compu4 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
